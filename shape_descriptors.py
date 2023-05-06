ab363fb72e19b8aeb524cf4a5a4318ce95ef49af
Marina <voronoi314@gmail.com>
2023-05-07 01:09:28 +0200

    implement curvature estimator

--- shape_descriptors.py
+++ shape_descriptors.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 from keras.layers import Layer
+from scipy.special import eval_hermitenorm
 
 
 @tf.function
 def soft_size(probabilities: tf.Tensor):
     rank = len(tf.shape(probabilities))
-    return tf.reduce_sum(probabilities, tuple(range(1, rank-1)))
+    return tf.reduce_sum(probabilities, tuple(range(1, rank - 1)))
 
 
 @tf.function
@@ -50,102 +51,109 @@ def gradient2d(image, st_element):
     """
     Gradient Operator
     """
-    dilation = tf.nn.dilation2d(image,
-                                st_element,
-                                (1, 1, 1, 1),
-                                'SAME',
-                                'NHWC',
-                                (1, 1, 1, 1))
-    erosion = tf.nn.erosion2d(image,
-                              st_element,
-                              (1, 1, 1, 1),
-                              'SAME',
-                              'NHWC',
-                              (1, 1, 1, 1))
+    dilation = tf.nn.dilation2d(
+        image, st_element, (1, 1, 1, 1), "SAME", "NHWC", (1, 1, 1, 1)
+    )
+    erosion = tf.nn.erosion2d(
+        image, st_element, (1, 1, 1, 1), "SAME", "NHWC", (1, 1, 1, 1)
+    )
     return dilation - erosion
 
 
+# TODO: implement multiscale version
 class CurvatureEstimator(Layer):
-    def __init__(self, num_classes, neighborhood_size, **kwargs):
+    """
+    Estimator of the curvature.
+    Based on the paper
+        Scale Space Edge Curvature Estimation and
+            Its Application to Straight Lines Detection
+        Ekaterina V. Semeikina, Dmitry V. Yurin
+    """
+
+    def __init__(self, num_classes, sigma, filter_width=None, **kwargs):
         super().__init__(**kwargs)
 
-        self.neighborhood_size = neighborhood_size
-        self.filter_size = 2 * neighborhood_size + 1
+        self.sigma = sigma
+        self.filter_width = filter_width
         self.num_classes = num_classes
 
-        kernel = tf.linspace(-neighborhood_size,
-                             neighborhood_size,
-                             self.filter_size)
-        kernel = tf.cast(kernel, tf.float32)
-        kernel = tf.expand_dims(kernel, 1)
-        kernel = tf.tile(kernel, (1, num_classes))
-        self.kernel_x = tf.reshape(kernel,
-                                   (self.filter_size, 1, num_classes, 1))
-        self.kernel_y = tf.reshape(kernel,
-                                   (1, self.filter_size, num_classes, 1))
+        self.sigma = sigma
+        if filter_width is None:
+            filter_width = int(4 * sigma)
+        self.width = filter_width
 
-        self.kernel_x_comp = tf.ones_like(self.kernel_y)
-        self.kernel_y_comp = tf.ones_like(self.kernel_x)
+        x = np.arange(-filter_width, filter_width + 1, dtype=np.float32)
+        g0 = np.exp(-(x**2) / (2 * sigma**2))
+        g0 /= np.sqrt(2 * np.pi * sigma)
 
-        self.kernel_x_sqr = self.kernel_x ** 2
-        self.kernel_y_sqr = self.kernel_y ** 2
+        g = [g0]
+        for n in range(1, 5):
+            tmp = (1 - 2 * (n % 2)) * eval_hermitenorm(n, x / sigma) * g0
+            tmp /= sigma**n
+            g.append(tmp)
+
+        self.gx = [p.reshape([-1, 1, 1, 1]) for p in g]
+        self.gy = [p.reshape([1, -1, 1, 1]) for p in g]
+
+    def build(self, input_shape):
+        num_channels = input_shape[0][-1]
+        self.gx = [np.concatenate(num_channels * [p], 2) for p in self.gx]
+        self.gy = [np.concatenate(num_channels * [p], 2) for p in self.gy]
+        self.gx = [tf.constant(p, dtype=tf.float32) for p in self.gx]
+        self.gy = [tf.constant(p, dtype=tf.float32) for p in self.gy]
+
+        super().build(input_shape)
+
+    def _get_derivatives(self, image):
+        out = list()
+        for n in [1, 3, 4]:
+            tmp_out = list()
+            for i in range(0, n + 1):
+                tmp = tf.nn.depthwise_conv2d(
+                    image, self.gx[n - i], (1, 1, 1, 1), "SAME"
+                )
+                tmp = tf.nn.depthwise_conv2d(
+                    tmp, self.gy[i], (1, 1, 1, 1), "SAME")
+                tmp_out.append(tmp)
+            out.append(tmp_out)
+        return out
 
     def get_config(self):
         config = super().get_config()
-        config['neighborhood_size'] = self.neighborhood_size
+        config["sigma"] = self.sigma
+        config["filter_width"] = self.filter_width
         return config
 
-    def call(self, edges):
-        tf.print(edges.shape)
-        offset_x = tf.nn.depthwise_conv2d(edges,
-                                          self.kernel_x,
-                                          [1, 1, 1, 1],
-                                          'SAME')
-        offset_x = tf.nn.depthwise_conv2d(offset_x,
-                                          self.kernel_x_comp,
-                                          (1, 1, 1, 1),
-                                          'SAME')
+    def call(self, inputs):
+        image, edges = inputs
+        derivs1, derivs3, derivs4 = self._get_derivatives(image)
+        ux, uy = derivs1
+        uxxx, uxxy, uxyy, uyyy = derivs3
+        uxxxx, uxxxy, uxxyy, uxyyy, uyyyy = derivs4
+        A = (uxxxx + uyyyy) * (ux**2) * (uy**2)
+        A += uxxyy * (ux**4 + uy**4 - 4 * (ux**2) * (uy**2))
+        A += 2 * ux * uy * (uy**2 - ux**2) * (uxxxy - uxyyy)
 
-        offset_y = tf.nn.depthwise_conv2d(edges,
-                                          self.kernel_y,
-                                          (1, 1, 1, 1),
-                                          'SAME')
-        offset_y = tf.nn.depthwise_conv2d(offset_y,
-                                          self.kernel_y_comp,
-                                          (1, 1, 1, 1),
-                                          'SAME')
+        B = tf.sqrt(ux**2 + uy**2)
+        B2 = uxxx * (ux**3) + uyyy * (uy**3)
+        B2 += 3 * ux * uy * (uxxy * ux + uxyy * uy)
+        B = B * B2
 
-        offset_norm = tf.sqrt(offset_x ** 2 + offset_y ** 2)
+        A = A * edges
+        B = B * edges + (1.0 - edges) * 1e-10
 
-        sqr_x = tf.nn.depthwise_conv2d(edges,
-                                       self.kernel_x_sqr,
-                                       (1, 1, 1, 1),
-                                       'SAME')
-        sqr_x = tf.nn.depthwise_conv2d(sqr_x,
-                                       self.kernel_x_comp,
-                                       (1, 1, 1, 1),
-                                       'SAME')
+        curv = -A / B
 
-        sqr_y = tf.nn.depthwise_conv2d(edges,
-                                       self.kernel_y_sqr,
-                                       (1, 1, 1, 1),
-                                       'SAME')
-        sqr_y = tf.nn.depthwise_conv2d(sqr_y,
-                                       self.kernel_y_comp,
-                                       (1, 1, 1, 1),
-                                       'SAME')
-        norm_sqr = sqr_x + sqr_y
-
-        return 2 * offset_norm / (norm_sqr + 1e-5)
+        return tf.abs(curv)
 
 
 if __name__ == "__main__":
     import numpy as np
 
     probs = np.zeros([1, 50, 50, 3], np.float32)
-    probs[0, 10:20, 10:20, 0] = 1.
-    probs[0, 25:30, 25:30, 1] = 1.
-    probs[..., 2] = 1. - probs[..., 0] - probs[..., 1]
+    probs[0, 10:20, 10:20, 0] = 1.0
+    probs[0, 25:30, 25:30, 1] = 1.0
+    probs[..., 2] = 1.0 - probs[..., 0] - probs[..., 1]
 
     # test soft_dist_centroid
     # sdc = soft_dist_centroid(probs)
@@ -155,18 +163,23 @@ if __name__ == "__main__":
     #
     # print(soft_size(probs))
 
-    from skimage.draw import disk
+    print("testing curvature")
     import matplotlib.pyplot as plt
-    probs = np.zeros([1, 50, 50, 2], np.float32)
-    ind_x, ind_y = disk((25, 25), 10, shape=(50, 50))
-    probs[0, ind_x, ind_y, 0] = 1.
-    probs[..., 1] = 1. - probs[..., 0]
+    from skimage.draw import disk
 
-    curv_est = CurvatureEstimator(2, 3)
-    grad = gradient2d(probs, tf.zeros([2, 2, 2]))
-    curv = curv_est(grad)
-    curv = np.array(curv)
-    grad = np.array(grad)
-    plt.imshow(grad[0, :, :, 0])
-    plt.show()
-    print(np.sum(curv, (1, 2)) / np.sum(grad, (1, 2)))
+    size = 151
+    curv_est = CurvatureEstimator(2, 3.0)
+    for r in [10, 20, 30, 40, 50, 60]:
+        print(f"radius = {r}, 1/radius = {1 / r}")
+        probs = np.zeros([1, size, size, 2], np.float32)
+        ind_x, ind_y = disk((size // 2, size // 2), r, shape=(size, size))
+        probs[0, ind_x, ind_y, 0] = 1.0
+        probs[..., 1] = 1.0 - probs[..., 0]
+        grad = gradient2d(probs, tf.zeros([3, 3, 2]))
+
+        curv = curv_est([probs, grad])
+        curv = np.array(curv)
+        grad = np.array(grad)
+        plt.imshow(curv[0, :, :, 0])
+        plt.show()
+        print(np.sum(curv, (1, 2)) / np.sum(grad, (1, 2)))
